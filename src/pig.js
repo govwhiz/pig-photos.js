b5fe48092d76bfd15bd444d4ab59cb9536c2b8ff
Evgeniy Krokhmal <ekrokhmal@cleverua.com>
2018-11-27 17:04:39 +0200

    fixed  enable function

--- src/pig.js
+++ src/pig.js
@@ -546,12 +546,12 @@
   Pig.prototype.enable = function() {
     // Find the container to load images into, if it exists.
     this.container = document.getElementById(this.settings.containerId);
-    this.scrollElement = this.container.parentElement;
     if (!this.container) {
       console.error('Could not find element with ID ' + this.settings.containerId);
       return;
     }
 
+    this.scrollElement = this.container.parentElement;
     this.onScroll = this._getOnScroll();
     this.scrollElement.addEventListener('scroll', this.onScroll);
 
